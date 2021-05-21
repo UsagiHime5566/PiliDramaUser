ed969a774385ed664104d66c8807222de2ab4393
nikki <moniqshiin@gmail.com>
2021-05-22 01:33:57 +0800

    匯入 app 首頁與 MRTK

---
+++ Assets/Scripts/UserSelection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserSelection : MonoBehaviour
+{
+    public List<Button> userButtons;
+    void Start()
+    {
+        int id = 0;
+        foreach (var item in userButtons)
+        {
+            int thisId = id;
+            item.onClick.AddListener(delegate {
+                Debug.Log($"select :{thisId}");
+            });
+            id++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
