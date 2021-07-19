d26be904f58bfdddcd337e4a6d6ed27aa8541a61
nikki <moniqshiin@gmail.com>
2021-07-19 13:10:57 +0800

    更新 Input 為 Fake Input

---
+++ Assets/Plugins/WebGLNativeInputField/Scripts/WebGLFakeInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebGLFakeInput : MonoBehaviour
+{
+    public InputField INP_Target;
+    public Button BTN_Source;
+    public string DialogTitle = "請輸入姓名";
+    public string Test_Input = "";
+    void Start()
+    {
+        BTN_Source.onClick.AddListener(PopDialog);
+    }
+
+    void PopDialog(){
+        #if UNITY_EDITOR
+            INP_Target.text = Test_Input;
+        #else
+            INP_Target.text = WebNativeDialog.OpenNativeStringDialog(DialogTitle, INP_Target.text);
+        #endif
+        Debug.Log("Fake BTN Event");
+    }
+}
